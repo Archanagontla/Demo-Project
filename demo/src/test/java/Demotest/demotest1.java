a76a58231641879daaa9cce59983d7c91c15fd3f
archa <archa@BhavanaKarre>
2025-05-09 10:33:40 +0530

    first commit

--- demo/src/test/java/Demotest/demotest1.java
+++ demo/src/test/java/Demotest/demotest1.java
@@ -2,16 +2,30 @@ package Demotest;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class demotest1 {
 
-	public static void main(String[] args) {
+	@Test
+	public void m1()
+	{
 		// TODO Auto-generated method stub
 
 				
 		//search  the product
 				WebDriver driver=new ChromeDriver();
 				driver.get("https://www.automationexercise.com/");
+				System.out.println(driver.getTitle());
+				try
+				{
+					Thread.sleep(5000);
+				}
+				catch(InterruptedException e)
+				{
+					e.printStackTrace();
+					
+				}
+				driver.close();
 				
 
 	}
